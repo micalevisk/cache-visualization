54f95e16e5af10ecd752e08ac9c3285f41759310
Arthur Wuterich <ArthurWut@gmail.com>
2015-04-20 21:34:03 -0700

    Created default cache structure

--- src/js/CacheSimulator.js
+++ src/js/CacheSimulator.js
@@ -1,9 +1,39 @@
-CacheSimulator = function( cacheSize, blockSize ) {
-  if( typeof cacheSize === "undefined" || blockSize === "undefined" ) {
+CacheSimulator = function( cacheSize, blockSize, setSize ) {
+  if( typeof cacheSize === "undefined" || 
+      typeof blockSize === "undefined" ||
+      typeof setSize   === "undefined"   ) {
     blockSize = 1;
     cacheSize = 10;
+    setSize = 1;
   }
 
   this.cacheSize = cacheSize;
   this.blockSize = blockSize;
+  this.setSize = setSize;
+  this.blocks = {};
+
+  // Setup cache blocks
+  for( var block = 0; block < this.cacheSize; block++ ) {
+    this.blocks[block] = {
+      data  : [],
+      lru   : 0,
+      tag   : "",
+      index : block
+    };
+
+    // Create all sets for block
+    for( var i = 0; i < this.setSize; i++ ) {
+      var dataBlock = [];
+
+      // Setup data blocks
+      for( var dataEntry = 0; dataEntry < this.blockSize; dataEntry++ ) {
+        dataBlock.push( "" );
+      }
+
+      this.blocks[block].data.push( dataBlock );
+    }
+  }
+}
+
+CacheSimulator.prototype.resolveRequest = function( address ) {
 }
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -6,12 +6,13 @@ app.controller('main', function($scope) {
   // Init main controller
   $scope.cacheSize = 10;
   $scope.blockSize = 1;
+  $scope.setSize   = 1;
   $scope.addresses = "1 10 34 8";
   $scope.cacheSimulator = new CacheSimulator();
 
   // Event handlers
   $scope.initCache = function() {
     console.log( "Init cache with the following properties: CacheSize: ", $scope.cacheSize, ", BlockSize: ", $scope.blockSize );
-    $scope.cacheSimulator = new CacheSimulator( $scope.cacheSize, $scope.blockSize );
+    $scope.cacheSimulator = new CacheSimulator( $scope.cacheSize, $scope.blockSize, $scope.setSize );
   }
 });
