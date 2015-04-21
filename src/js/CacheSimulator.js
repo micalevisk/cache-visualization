9c951a0feb6f4629482459c56a9cfb1e60a4650b
Arthur Wuterich <arthurwut@gmail.com>
2015-04-21 11:23:58 -0700

    Corrects incorrect block-set ordering

--- src/js/CacheSimulator.js
+++ src/js/CacheSimulator.js
@@ -1,39 +1,74 @@
+function nearestPowerOfTwo( val ) {
+  var result = 0;
+  
+  result = Math.pow(2,Math.round(Math.log(parseInt(val))/Math.log(2)));
+
+  return result;
+}
+
+function decToBin(dec) {
+    return (dec >>> 0).toString(2);
+}
+
+function padLeft(nr, n, str){
+    return Array(n-String(nr).length+1).join(str||'0')+nr;
+}
+
 CacheSimulator = function( cacheSize, blockSize, setSize ) {
   if( typeof cacheSize === "undefined" || 
       typeof blockSize === "undefined" ||
       typeof setSize   === "undefined"   ) {
     blockSize = 1;
-    cacheSize = 10;
+    cacheSize = 8;
     setSize = 1;
   }
 
-  this.cacheSize = cacheSize;
-  this.blockSize = blockSize;
-  this.setSize = setSize;
-  this.blocks = {};
+  this.cacheSize = nearestPowerOfTwo( cacheSize );
+  this.blockSize = nearestPowerOfTwo( blockSize );
+  this.setSize = nearestPowerOfTwo( setSize );
 
-  // Setup cache blocks
-  for( var block = 0; block < this.cacheSize; block++ ) {
-    this.blocks[block] = {
-      data  : [],
-      lru   : 0,
-      tag   : "",
-      index : block
-    };
+  // Clamp the setSize to be within the maximum blocksize
+  if( this.setSize > this.cacheSize) {
+    this.setSize = this.cacheSize;
+  }
+  this.sets = []
 
-    // Create all sets for block
-    for( var i = 0; i < this.setSize; i++ ) {
-      var dataBlock = [];
+  // Add the correct number of sets to the cache
+  for( var setIndex = 0; setIndex < (this.cacheSize/this.setSize); setIndex++ ) {
+    var set = {
+      index  : setIndex,
+      lru    : 0,
+      blocks : [],
+    }
 
-      // Setup data blocks
-      for( var dataEntry = 0; dataEntry < this.blockSize; dataEntry++ ) {
-        dataBlock.push( "" );
+    for( var block = 0; block < this.setSize; block++ ) {
+      var data = [];
+      for( var i = 0; i < this.blockSize; i++ ) {
+        data.push("-");
       }
 
-      this.blocks[block].data.push( dataBlock );
+      set.blocks.push( {
+        index : block,
+        tag   : "",
+        data  : data
+      });
     }
+    this.sets.push( set );
   }
+
 }
 
 CacheSimulator.prototype.resolveRequest = function( address ) {
+  var bin = padLeft(decToBin(address),32);
+  console.log( address, bin );
+  // Find the correct set
+  // Find the correct block
+  // Check to see if the data is within the block by comparing tags from each block in the set
+  // Fill in the correcy data block
+  // Fill in the rest of the data-block if blocksize>1
+  // Update LRU for the entire set
 }
+
+
+
+
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -4,15 +4,24 @@
 
 app.controller('main', function($scope) {
   // Init main controller
-  $scope.cacheSize = 10;
+  $scope.cacheSize = 8;
   $scope.blockSize = 1;
   $scope.setSize   = 1;
-  $scope.addresses = "1 10 34 8";
+  $scope.addresses = "";
   $scope.cacheSimulator = new CacheSimulator();
 
   // Event handlers
   $scope.initCache = function() {
-    console.log( "Init cache with the following properties: CacheSize: ", $scope.cacheSize, ", BlockSize: ", $scope.blockSize );
+    console.log( "Init cache with the following properties: CacheSize: ", $scope.cacheSize, ", BlockSize: ", $scope.blockSize, ", SetSize: ", $scope.setSize );
     $scope.cacheSimulator = new CacheSimulator( $scope.cacheSize, $scope.blockSize, $scope.setSize );
+  }
+
+  $scope.processAddress = function() {
+    if( $scope.addresses.length ) {
+      console.log( "Process the provided address: ", $scope.addresses );
+      $scope.cacheSimulator.resolveRequest( $scope.addresses );
+    }
+
+    $scope.addresses = "";
   }
 });
